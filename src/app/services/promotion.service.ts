cac75acf9ef91183ff14df0c433734d13302ba87
Suiyi Fu <fusuiyi123@gmail.com>
2018-04-05 20:39:10 -0500

    Simulate short delay in obtaining the results via Promise

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { resolve } from 'url';
 
 @Injectable()
 export class PromotionService {
@@ -9,15 +10,20 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
 
   getPromotion(id: number): Promise<Promotion> {
-
-    return Promise.resolve(PROMOTIONS.filter((promotion) => (promotion.id == id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id == id))[0]), 2000);
+    });
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured == true)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured == true)[0]), 2000);
+    })
   }
 }
